07e82cc28e57e1a9c9b9784a283dfec9f4c19243
Dale Manthei <dalemanthei@users.noreply.github.com>
2017-04-01 19:34:08 -0500

    feat(giphy): add giphy search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
+import { Ng2RestAppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { GiphyService } from './searchbox/giphy.service';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchboxComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    JsonpModule,
+    MaterialModule,
+    Ng2RestAppRoutingModule,
+    StoreModule.provideStore({ /* reducers here */ })
   ],
-  providers: [],
+  providers: [GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
