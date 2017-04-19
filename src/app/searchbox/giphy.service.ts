a7fd47c309c7517e816f08341b1edaa58f0c91dd
Dale Manthei <dalemanthei@users.noreply.github.com>
2017-04-19 17:53:02 -0500

    feat(search): update ui

--- src/app/searchbox/giphy.service.ts
+++ src/app/searchbox/giphy.service.ts
@@ -10,19 +10,21 @@ export class GiphyService {
   ) {}
 
   search (term: string) {
-    let search = new URLSearchParams();
+    const search = new URLSearchParams();
     search.set('api_key', 'dc6zaTOxFJmzC');
     search.set('q', term);
     search.set('rating', 'g');
-    search.set('limit', '10');
+    search.set('limit', '20');
 
     return this.http
       .get('http://api.giphy.com/v1/gifs/search', { search })
       .map(res => res.json()['data'])
+      // .do(g => console.log(g))
       .map(image =>
         image.map(g => ({
           still: g.images.fixed_width_still,
           small: g.images.fixed_width_small,
+          downsized: g.images.preview_gif,
           large: g.images.downsized_large
         })))
       ;
